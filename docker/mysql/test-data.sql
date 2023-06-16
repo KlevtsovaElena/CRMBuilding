309bab48402ed787c4b076e2916b4e9c34bba76c
StacyMch <greensummertea@gmail.com>
2023-06-16 18:13:04 +0300

    C001-3

--- docker/mysql/test-data.sql
+++ docker/mysql/test-data.sql
@@ -7,14 +7,120 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+DROP TABLE IF EXISTS `brands`;
+CREATE TABLE `brands` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `brand_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `brands` (`id`, `brand_name`) VALUES
+(1,	'Форус'),
+(2,	'AZIA'),
+(3,	'КНАУФ'),
+(4,	'VERO');
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `categories` (`id`, `category_name`) VALUES
+(1,	'потолочный'),
+(2,	'стеновой'),
+(3,	'обычный'),
+(4,	'категория 4');
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE `customers` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `last_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `phone` smallint DEFAULT NULL,
+  `city` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `ordered_product`;
+CREATE TABLE `ordered_product` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` int unsigned NOT NULL,
+  `product_id` int unsigned NOT NULL,
+  `size_id` int unsigned NOT NULL,
+  `quantity` int unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `customer_id` int unsigned NOT NULL,
+  `order_date` timestamp NOT NULL,
+  `brand_id` int unsigned NOT NULL,
+  `ordered_products` json NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `brand_id` (`brand_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `price_changes`;
+CREATE TABLE `price_changes` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int unsigned NOT NULL,
+  `date_change` timestamp NOT NULL,
+  `new_price` int unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8mb3 NOT NULL,
+  `description` text CHARACTER SET utf8mb3 NOT NULL,
+  `photo` varchar(300) CHARACTER SET utf8mb3 NOT NULL,
+  `category_id` int unsigned NOT NULL,
+  `type_id` int unsigned DEFAULT NULL,
+  `brand_id` int unsigned NOT NULL,
+  `quantity_available` int unsigned DEFAULT NULL,
+  `price` int unsigned NOT NULL,
+  `market_price` int unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `products` (`id`, `name`, `description`, `photo`, `category_id`, `type_id`, `brand_id`, `quantity_available`, `price`, `market_price`) VALUES
+(1,	'Гипсокартон',	'Описание',	'ссылка',	1,	NULL,	3,	NULL,	45000,	50000);
+
+DROP TABLE IF EXISTS `sizes`;
+CREATE TABLE `sizes` (
+  `id` int unsigned NOT NULL,
+  `size_value` int unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 DROP TABLE IF EXISTS `test`;
 CREATE TABLE `test` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `test` (`id`, `name`) VALUES
 (1,	'test row');
 
--- 2023-06-16 08:36:11
+DROP TABLE IF EXISTS `types`;
+CREATE TABLE `types` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `types` (`id`, `type_name`) VALUES
+(1,	'влагостойкий');
+
+-- 2023-06-16 14:41:53
