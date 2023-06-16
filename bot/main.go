889016ca117664563cb0ddfd1c01d9f8bf9f63fb
Logunov Igor <logunov.igor94@icloud.com>
2023-06-16 12:18:11 +0300

    C001-4. Подготовил инфраструктуру.

---
+++ bot/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Hello, Docker! <3 from Go.")
+	})
+
+	httpPort := "8080"
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
