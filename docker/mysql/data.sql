b894a5135e24990d530f15da3af3b61806d8ec0d
StacyMch <greensummertea@gmail.com>
2023-06-19 17:56:53 +0300

    C001-22

--- docker/mysql/data.sql
+++ docker/mysql/data.sql
@@ -27,6 +27,11 @@ CREATE TABLE `categories` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `categories` (`id`, `category_name`) VALUES
+(1,	'Гипсокартон'),
+(2,	'Штукатурка'),
+(3,	'Шпаклевка'),
+(4,	'Грунтовка');
 
 DROP TABLE IF EXISTS `cities`;
 CREATE TABLE `cities` (
@@ -35,6 +40,21 @@ CREATE TABLE `cities` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `cities` (`id`, `name`) VALUES
+(1,	'Ташкент'),
+(2,	'Нурафшон'),
+(3,	'Бухара'),
+(4,	'Самарканд'),
+(5,	'Карши'),
+(6,	'Термез'),
+(7,	'Навои'),
+(8,	'Джизак'),
+(9,	'Гулистан'),
+(10,	'Андижан'),
+(11,	'Наманган'),
+(12,	'Фергана'),
+(13,	'Угренч'),
+(14,	'Нукус');
 
 DROP TABLE IF EXISTS `commissioners`;
 CREATE TABLE `commissioners` (
@@ -54,7 +74,7 @@ CREATE TABLE `customers` (
   `first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `last_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `tg_username` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `tg_id` bigint unsigned DEFAULT NULL,
+  `tg_id` bigint unsigned NOT NULL,
   `phone` smallint NOT NULL,
   `city_id` int unsigned NOT NULL,
   `coordinates` json DEFAULT NULL,
@@ -81,7 +101,7 @@ CREATE TABLE `orders` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `customer_id` int unsigned NOT NULL,
   `order_date` timestamp NOT NULL,
-  `ordered_products` json NOT NULL,
+  `ordered_products` json DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -103,7 +123,7 @@ CREATE TABLE `products` (
   `description` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
   `photo` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `category_id` int unsigned NOT NULL,
-  `brand_id` int unsigned NOT NULL,
+  `brand_id` int unsigned DEFAULT NULL,
   `vendor_id` int unsigned DEFAULT NULL,
   `quantity_available` int unsigned DEFAULT NULL,
   `price` int unsigned DEFAULT NULL,
@@ -112,13 +132,30 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `products` (`id`, `name`, `description`, `photo`, `category_id`, `brand_id`, `vendor_id`, `quantity_available`, `price`, `market_price`) VALUES
-(1,	'Гипсокартон',	'Описание',	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	0,	NULL,	NULL,	45000,	50000);
+(1,	'Гипсокартон ФОРУС стеновой простой (12.5мм) (2 сорт)',	'Описание',	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	1,	NULL,	NULL,	49000,	50000),
+(2,	'Гипсокартон ФОРУС стеновой влагостойкий (12.5мм) (2 сорт)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	1,	NULL,	NULL,	51000,	NULL),
+(3,	'Гипсокартон ФОРУС потолочный влагостойкий (9.5мм) (2 сорт)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	1,	NULL,	NULL,	50000,	NULL),
+(4,	'Гипсокартон ФОРУС стеновой простой (12.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	1,	NULL,	NULL,	56000,	NULL),
+(5,	'Гипсокартон ФОРУС паталочный влагастойкий (9.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	1,	NULL,	NULL,	58000,	NULL),
+(6,	'Гипсокартон ФОРУС потолочный простой (9.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	1,	NULL,	NULL,	49000,	NULL),
+(7,	'Гипсокартон ФОРУС стеновой влагастойкий (12.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	1,	NULL,	NULL,	63000,	NULL),
+(8,	'Гипсокартон AZIA потолочный простой (9.5мм)',	NULL,	NULL,	1,	2,	NULL,	NULL,	49000,	NULL),
+(9,	'Гипсокартон AZIA стеновой простой (12.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	2,	NULL,	NULL,	56000,	NULL),
+(10,	'Гипсокартон AZIA потолочный влагостойкий (9.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	2,	NULL,	NULL,	60000,	NULL),
+(11,	'Гипсокартон AZIA стеновой влагостойкий (12.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	2,	NULL,	NULL,	69000,	NULL),
+(12,	'Гипсокартон КНАУФ потолочный простой (9.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	3,	NULL,	NULL,	52200,	NULL),
+(13,	'Гипсокартон КНАУФ стеновой простой (12.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	3,	NULL,	NULL,	59300,	NULL),
+(14,	'Гипсокартон КНАУФ потолочный влагостойкий (9.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	3,	NULL,	NULL,	67300,	NULL),
+(15,	'Гипсокартон КНАУФ стеновой влагостойкий (12.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	3,	NULL,	NULL,	74200,	NULL),
+(16,	'Гипсокартон VERO стеновой влагостойкий (12.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	4,	NULL,	NULL,	46000,	NULL),
+(17,	'Гипсокартон VERO стеновой простой (12.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	NULL,	4,	NULL,	46500,	NULL),
+(18,	'Гипсокартон VERO потолочный простой (8.5мм)',	NULL,	'https://cdn.leroymerlin.ru/lmru/image/upload/v1613641220/b_white,c_pad,d_photoiscoming.png,f_auto,h_2000,q_auto,w_2000/lmcode/T8QNN_Bj2Uq3Zvaw8pHo6Q/10072681.jpg',	1,	4,	NULL,	NULL,	41500,	NULL);
 
 DROP TABLE IF EXISTS `vendors`;
 CREATE TABLE `vendors` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
-  `city_id` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `city_id` int unsigned NOT NULL,
   `phone` smallint unsigned DEFAULT NULL,
   `email` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `tg_username` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -128,4 +165,4 @@ CREATE TABLE `vendors` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
--- 2023-06-19 07:32:08
+-- 2023-06-19 14:55:15
