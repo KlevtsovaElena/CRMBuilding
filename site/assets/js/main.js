fa89e871ac5fe58d892cd17e37ac05969c94a927
chemezoida <evchemez@mail.ru>
2023-06-21 13:20:01 +0300

    c001-6

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -26,8 +26,8 @@ const menuLeft = document.querySelector('.menu-left');
 const mainContent = document.querySelector('.main-content');
 
 function toggleMenu() {
-    menuLeft.classList.toggle('collapse');
-    mainContent.classList.toggle('collapse');
+    menuLeft.classList.toggle('collapsed');
+    mainContent.classList.toggle('collapsed');
 }
 
 
@@ -44,30 +44,14 @@ async function addProduct() {
     //валидация
 
 
-
-// вариант 1
-    let formData = new FormData(formAddProduct);
-    let response = await fetch('test.php', {
-        method: 'POST',
-        body: formData
-    });
-
-    if (response.ok){
-        let result = await response.json();
-    } else {
-        return "Ошибка!";
-    }
-
-
-// вариант 2
-    const url = "http://localhost/pages/test.php";
-    let json = sendRequestPOST(url, params);
-    let data = JSON.parse(json);
+    // const url = "http://localhost/pages/test.php";
+    // let json = sendRequestPOST(url, params);
+    // let data = JSON.parse(json);
 
 
     
-    console.log(response);
-    console.log(data);
+    console.log("отправлено");
+
 
 }
 
