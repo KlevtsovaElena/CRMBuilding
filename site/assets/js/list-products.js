9615f31514250e9772bf52e0c3b1b3527e23eaaf
chemezoida <evchemez@mail.ru>
2023-06-24 21:56:28 +0300

    C001-9-10

--- site/assets/js/list-products.js
+++ site/assets/js/list-products.js
@@ -1,24 +1,196 @@
 console.log('подключили list-products');
+// определим основные переменные
 let currentPage = 1;
-let vendor_id = document.getElementById('vendor_id').value;
-let limitEl = document.getElementById('limit');
-let totalProductsEl = document.querySelectorAll('.list-products__row');
-
-
-let limit = limitEl.value
-let totalProducts = totalProductsEl.length;
-
-let brand_idEl = document.getElementById('brand_id');
-let category_idEl = document.getElementById('category_id');
-let searchEl = document.getElementById('search');
-let offsetEl = document.getElementById('offset');
-let orderbyEl = document.getElementById('orderby');
-
+let params;
 let prevButton;
 let nextButton;
 let totalPages;
+let brands = {};
+let categories = {};
+let vendor_id = document.getElementById('vendor_id').value;
+let brand_idEl = document.getElementById('brand_id');
+let category_idEl = document.getElementById('category_id');
+let searchEl = document.getElementById('search');
+let limitEl = document.getElementById('limit');
+let limit = limitEl.value
 
-renderPagination(totalProducts, limit);
+// соберём значения брендов и категорий
+brand_idEl.querySelectorAll('option').forEach(item => {
+    brands[item.value] = item.innerText;
+})
+category_idEl.querySelectorAll('option').forEach(item => {
+    categories[item.value] = item.innerText;
+})
+
+// получение записей товаров из БД
+let totalProductsJson = sendRequestGET('http://localhost/api/products.php?vendor_id=' + vendor_id);
+let totalProducts = JSON.parse(totalProductsJson);
+
+// подсчёт полученных записей
+let totalProductsCount = totalProducts.length;
+
+// отрисуем пагинацию
+renderPagination(totalProductsCount, limit);
+
+// отрисуем товары в таблице 
+renderListProducts(totalProducts);
+
+
+/* ---------- ОТРИСОВКА ТОВАРОВ В ТАБЛИЦЕ---------- */
+function renderListProducts(totalProducts) {
+    
+    // найдём шаблон и контейнер для отрисовки
+    const tmplRowProduct = document.getElementById('template-body-table').innerHTML;
+    const containerListProducts = document.querySelector('.list-products__body');
+
+    // очистим контейнер
+    containerListProducts.innerHTML = "";
+
+    // если записей нет, то выводим об этом инфо и выходим
+    if (totalProducts.length === 0) {
+        const info = document.querySelector('.info-table');
+        info.innerText = "Записей с такими параметрами нет";
+        return;
+    }
+
+    // заполним данными и отрисуем шаблон
+    for (i = 0; i < limit; i++) {
+        containerListProducts.innerHTML += tmplRowProduct.replace('${article}', totalProducts[i]['article'])
+                                                        .replace('${photo}',  totalProducts[i]['photo'])
+                                                        .replace('${name}', totalProducts[i]['name'])
+                                                        .replace('${brand_id}', brands[totalProducts[i]['brand_id']])
+                                                        .replace('${category_id}', categories[totalProducts[i]['category_id']])
+                                                        .replace('${quantity_available}', totalProducts[i]['quantity_available'])
+                                                        .replace('${price}', totalProducts[i]['price'])
+                                                        .replace('${max_price}', totalProducts[i]['max_price']);
+    }
+}
+
+
+/* ---------- ОТРИСОВКА ПАГИНАЦИИ ---------- */
+function renderPagination(totalProductsCount, limit) {
+
+    // из полученных переменных получаем кол-во страниц
+    totalPages = Math.ceil(totalProductsCount/limit);
+
+    // найдём шаблон и контейнер для отрисовки
+    const tmplPagination = document.getElementById('template-pagination').innerHTML;
+    const containerPagination = document.querySelector('.pagination-wrapper');
+
+    // очистим контейнер
+    containerPagination.innerHTML = "";
+
+    // заполним данными и отрисуем шаблон
+    containerPagination.innerHTML = tmplPagination  .replace('${currentPage}', currentPage)
+                                                    .replace('${currentPage}', currentPage)
+                                                    .replace('${totalPages}', totalPages);
+
+    prevButton = document.querySelector('.page-switch__prev');
+    nextButton = document.querySelector('.page-switch__next');
+
+    // если количество страниц>1, то делаем активной кнопку далее
+    if (totalPages > 1) {
+        nextButton.removeAttribute('disabled');
+    }
+
+    console.log('totalPages', totalPages);
+}
+
+
+/* ---------- ПЕРЕКЛЮЧЕНИЕ СТРАНИЧЕК ---------- */
+function switchPage(variance) {
+
+    // 1. поменяем номер странички
+    currentPage = currentPage + variance;
+
+    let containerCurrentPage = document.querySelectorAll('.current-page');
+    containerCurrentPage.forEach(item => {
+        item.innerText = currentPage;
+    })
+
+    // 2. настроим возможность/невозможность переключения страниц 
+    if (currentPage === 1) {
+       prevButton.setAttribute('disabled', '');
+       if (totalPages > 1) {
+            nextButton.removeAttribute('disabled');
+       }
+    } else if (currentPage > 1) {
+        prevButton.removeAttribute('disabled');
+
+        if (currentPage === totalPages) {
+            nextButton.setAttribute('disabled', '');
+        }
+    }
+   
+    // соберём строку запроса
+    params = "&brand_id=2"
+
+    // отправим запрос
+    totalProductsJson = sendRequestGET('http://localhost/api/products.php?vendor_id=111' + params);
+    totalProducts = JSON.parse(totalProductsJson);
+
+    // отрисуем таблицу
+    renderListProducts(totalProducts)
+}
+
+
+/* ---------- СОБЕРЁМ СТРОКУ ЗАПРОСА ---------- */
+function getRequestParams() {
+    // собираем все значения полей
+    params = "vendor_id=" + vendor_id;
+
+    [brand_idEl, category_idEl, searchEl, limitEl, orderbyEl].forEach(item => {
+currentPage=2;
+
+        if(item.value.trim()) {
+
+            if (item.id === "search") {
+                params += "&search=name:" + searchEl.value + ";description:" + searchEl.value;
+            } else if (item.id === "orderby") {
+                params += "&orderby=" + item.value + ":asc"; 
+            }else {
+                    params += "&" + item.id + "=" + item.value;  
+            }
+        }
+
+        if ((item.id === "limit") && !(currentPage===1)) {
+            params += "&offset=" + ((currentPage-1)*limitEl.value + 1);
+        }
+    })
+
+console.log(params);
+    // if(!(searchEl.value.trim())) {
+    //     let search = "name:" + searchEl.value + ";description:" + searchEl.value;
+    // }
+
+    // if(!(limit.value)) {
+        
+    // }
+    
+
+
+
+    // totalProducts = totalProductsEl.length;
+    
+
+
+
+
+
+
+
+    // разбираем значения полей
+
+    // получаем строку get параметров запроса
+    sendRequestGET('http://localhost/api/products?' + params);
+    // отправляем запрос на сервер
+
+
+    // получаем данные
+
+    // перепишем totalProducts 
+}
+
 
 /* ---------- НАЖАТИЕ НА ПРИМЕНИТЬ ---------- */
 const sendChangeData = document.querySelector('.form-filters').querySelector('button');
@@ -68,112 +240,3 @@ headTableProducts.forEach(item => {
     item.addEventListener("click", sortChange);
 })
 
-
-
-
-/* ---------- ОТРИСОВКА ТАБЛИЦЫ ТОВАРОВ ---------- */
-function renderListProducts() {
-    // собираем все значения полей
-    let params = "vendor_id=" + vendor_id;
-
-    [brand_idEl, category_idEl, searchEl, limitEl, orderbyEl].forEach(item => {
-currentPage=2;
-
-        if(item.value.trim()) {
-
-            if (item.id === "search") {
-                params += "&search=name:" + searchEl.value + ";description:" + searchEl.value;
-            } else if (item.id === "orderby") {
-                params += "&orderby=" + item.value + ":asc"; 
-            }else {
-                    params += "&" + item.id + "=" + item.value;  
-            }
-        }
-
-        if ((item.id === "limit") && !(currentPage===1)) {
-            params += "&offset=" + ((currentPage-1)*limitEl.value + 1);
-        }
-    })
-
-console.log(params);
-    // if(!(searchEl.value.trim())) {
-    //     let search = "name:" + searchEl.value + ";description:" + searchEl.value;
-    // }
-
-    // if(!(limit.value)) {
-        
-    // }
-    
-
-
-
-    totalProducts = totalProductsEl.length;
-    
-
-
-
-
-
-
-
-    // разбираем значения полей
-
-    // получаем строку get параметров запроса
-    sendRequestGET('http://localhost/api/products?' + params);
-    // отправляем запрос на сервер
-
-
-    // получаем данные
-
-    // перепишем totalProducts 
-}
-
-
-/* ---------- ОТРИСОВКА ПАГИНАЦИИ ---------- */
-function renderPagination(totalProducts, limit) {
-
-    // из полученных переменных получаем кол-во страниц
-    totalPages = Math.ceil(totalProducts/limit);
-
-    // найдём шаблон и контейнер для отрисовки
-    const tmplPagination = document.getElementById('template-pagination').innerHTML;
-    const containerPagination = document.querySelector('.pagination-wrapper');
-
-    // очистим контейнер
-    containerPagination.innerHTML = "";
-
-    // заполним данными и отрисуем шаблон
-    containerPagination.innerHTML = tmplPagination  .replace('${currentPage}', currentPage)
-                                                    .replace('${currentPage}', currentPage)
-                                                    .replace('${totalPages}', totalPages);
-
-    
-    prevButton = document.querySelector('.page-switch__prev');
-    nextButton = document.querySelector('.page-switch__next');
-    console.log('totalPages', totalPages);
-}
-
-/* ---------- ПЕРЕКЛЮЧЕНИЕ СТРАНИЧЕК ---------- */
-function switchPage(variance) {
-
-    let containerCurrentPage = document.querySelector('.current-page');
-
-    currentPage = currentPage + variance;
-
-    if (currentPage === 1) {
-       prevButton.setAttribute('disabled', '');
-       if (totalPages > 1) {
-            nextButton.removeAttribute('disabled');
-       }
-    } else if (currentPage > 1) {
-        prevButton.removeAttribute('disabled');
-
-        if (currentPage === totalPages) {
-            nextButton.setAttribute('disabled', '');
-        }
-    }
-
-
-    containerCurrentPage.innerText = currentPage;
-    
-}
