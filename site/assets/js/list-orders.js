bf0f07b8e4afb641a18eb2e4dd27bdad924f8b61
chemezoida <evchemez@mail.ru>
2023-06-27 13:47:31 +0300

    C001-36 наброски

--- site/assets/js/list-orders.js
+++ site/assets/js/list-orders.js
@@ -1,6 +1,347 @@
+const containerListOrders2 = document.querySelector('.list-orders__body2');
+let testStatus = {
+    "0": "Новый",
+    "1": "Просмотрен",
+    "2": "Подтверждён",
+    "3": "Отменён",
+    "4": "Звершён",
+    "5": "Не дозвонились"
+
+}
+let testData = [
+    {
+        "id": 2,
+        "order_id": 124,
+        "vendor_id": 1,
+        "order_date": 1235664544,
+        "status": 0,
+        "products":
+            [
+                {
+                    "id": 15,
+                    "quantity": 4,
+                    "name": "Гипсокартон1",
+                    "price": 100,
+                    "available": 6
+                },
+                {
+                    "id": 1,
+                    "quantity": 2,
+                    "name": "Гипсокартон jhskjfhaskjhfaskjh",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 14,
+                    "quantity": 1,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 5,
+                    "quantity": 15,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 56
+                }   
+            ],
+        "comment": "Комментарий",
+        "complete_date": 0,
+        "customers": {
+            "phone": 42454545,
+            "location": 
+            {
+            
+                "latitude": 55.657107, 
+                "longitude": 37.569608
+            }
+        },
+        "vendor_location": {
+            "latitude": 55.657107, 
+            "longitude": 37.569608
+        }
+    },
+
+    {
+        "id": 3,
+        "order_id": 134,
+        "vendor_id": 1,
+        "order_date": 1235664544,
+        "status": 1,
+        "products":
+            [
+                {
+                    "id": 15,
+                    "quantity": 4,
+                    "name": "Гипсокартон2",
+                    "price": 100,
+                    "available": 6
+                },
+                {
+                    "id": 1,
+                    "quantity": 2,
+                    "name": "Гtdytetyuety",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 14,
+                    "quantity": 1,
+                    "name": "Штукатурочка2",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 5,
+                    "quantity": 15,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 56
+                }   
+            ],
+        "comment": "Комментарий",
+        "complete_date": 0,
+        "customers": {
+            "phone": 42454545,
+            "location": 
+            {
+            
+                "latitude": 55.657107, 
+                "longitude": 37.569608
+            }
+        },
+        "vendor_location": {
+            "latitude": 55.657107, 
+            "longitude": 37.569608
+        }
+    },
+
+    {
+        "id": 5,
+        "order_id": 126,
+        "vendor_id": 1,
+        "order_date": 1235664544,
+        "status": 2,
+        "products":
+            [
+                {
+                    "id": 15,
+                    "quantity": 4,
+                    "name": "Гипсокартон1",
+                    "price": 100,
+                    "available": 6
+                },
+                {
+                    "id": 1,
+                    "quantity": 2,
+                    "name": "Гипсокартон jhskjfhaskjhfaskjh",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 14,
+                    "quantity": 1,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 5,
+                    "quantity": 15,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 56
+                }   
+            ],
+        "comment": "Комментарий",
+        "complete_date": 0,
+        "customers": {
+            "phone": 42454545,
+            "location": 
+            {
+            
+                "latitude": 55.657107, 
+                "longitude": 37.569608
+            }
+        },
+        "vendor_location": {
+            "latitude": 55.657107, 
+            "longitude": 37.569608
+        }
+    },
+
+    {
+        "id": 14,
+        "order_id": 154,
+        "vendor_id": 1,
+        "order_date": 1235664544,
+        "status": 3,
+        "products":
+            [
+                {
+                    "id": 15,
+                    "quantity": 4,
+                    "name": "Гипсокартон1",
+                    "price": 100,
+                    "available": 6
+                },
+                {
+                    "id": 1,
+                    "quantity": 2,
+                    "name": "Гипсокартон jhskjfhaskjhfaskjh",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 14,
+                    "quantity": 1,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 5,
+                    "quantity": 15,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 56
+                }   
+            ],
+        "comment": "Комментарий",
+        "complete_date": 0,
+        "customers": {
+            "phone": 42454545,
+            "location": 
+            {
+            
+                "latitude": 55.657107, 
+                "longitude": 37.569608
+            }
+        },
+        "vendor_location": {
+            "latitude": 55.657107, 
+            "longitude": 37.569608
+        }
+    },
+
+    {
+        "id": 14,
+        "order_id": 154,
+        "vendor_id": 1,
+        "order_date": 1235664544,
+        "status": 4,
+        "products":
+            [
+                {
+                    "id": 15,
+                    "quantity": 4,
+                    "name": "Гипсокартон1",
+                    "price": 100,
+                    "available": 6
+                },
+                {
+                    "id": 1,
+                    "quantity": 2,
+                    "name": "Гипсокартон jhskjfhaskjhfaskjh",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 14,
+                    "quantity": 1,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 5,
+                    "quantity": 15,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 56
+                }   
+            ],
+        "comment": "Комментарий",
+        "complete_date": 45346546546,
+        "customers": {
+            "phone": 42454545,
+            "location": 
+            {
+            
+                "latitude": 55.657107, 
+                "longitude": 37.569608
+            }
+        },
+        "vendor_location": {
+            "latitude": 55.657107, 
+            "longitude": 37.569608
+        }
+    },
+
+    {
+        "id": 14,
+        "order_id": 154,
+        "vendor_id": 1,
+        "order_date": 1235664544,
+        "status": 5,
+        "products":
+            [
+                {
+                    "id": 15,
+                    "quantity": 4,
+                    "name": "Гипсокартон1",
+                    "price": 100,
+                    "available": 6
+                },
+                {
+                    "id": 1,
+                    "quantity": 2,
+                    "name": "Гипсокартон jhskjfhaskjhfaskjh",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 14,
+                    "quantity": 1,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 3
+                },
+                {
+                    "id": 5,
+                    "quantity": 15,
+                    "name": "Штукатурочка",
+                    "price": 100,
+                    "available": 56
+                }   
+            ],
+        "comment": "Комментарий",
+        "complete_date": 45346546546,
+        "customers": {
+            "phone": 42454545,
+            "location": 
+            {
+            
+                "latitude": 55.657107, 
+                "longitude": 37.569608
+            }
+        },
+        "vendor_location": {
+            "latitude": 55.657107, 
+            "longitude": 37.569608
+        }
+    }
+
+]
+
+
+
+
 console.log('подключили list-orders');
 let vendor_id = document.getElementById('vendor_id').value;
 const containerListOrders = document.querySelector('.list-orders__body');
+
 // найдём шаблон и контейнер для отрисовки
 const tmplRowOrder = document.getElementById('template-body-table').innerHTML;
 
@@ -67,10 +408,12 @@ function renderListOrders(totalOrders) {
                                                         .replace('${order_id}', totalOrders[i]['order_id'])
                                                         .replace('${id}', totalOrders[i]['id'])
                                                         .replace('${status}', totalOrders[i]['status'])
+                                                        .replace('${status}', totalOrders[i]['status'])
+                                                        .replace('${status}', totalOrders[i]['status'])
                                                         .replace('${order_date}', "")
                                                         .replace('${products}', productsListInOrder)
                                                         .replace('${total_price}', "")
-                                                        .replace('${complete_date}', "")
+                                                        .replace('${complete_date}', "");
 
     }
 }
@@ -103,4 +446,47 @@ function renderPagination(totalProductsCount, limit) {
     }
 
     console.log('totalPages', totalPages);
+}
+
+test() ;
+
+function test() {
+
+    console.log(testData);
+    for (let i = 0; i < testData.length; i++) {
+
+        let productsTest = "";
+        let totalPriceTest = 0; 
+
+        for (let j = 0; j < testData[i]['products'].length; j++){
+            productsTest += testData[i]['products'][j]['name'] + " (" + 
+                            (testData[i]['products'][j]['quantity']) + '), ';
+            totalPriceTest = testData[i]['products'][j]['quantity'] * testData[i]['products'][j]['price'];
+
+        }
+
+
+
+        containerListOrders2.innerHTML += tmplRowOrder.replace('${order_id}', testData[i]['order_id'])
+                                                        .replace('${order_id}', testData[i]['order_id'])
+                                                        .replace('${id}', testData[i]['id'])
+                                                        .replace('${status}', testData[i]['status'])
+                                                        .replace('${status}', testData[i]['status'])
+                                                        .replace('${status}', testStatus[testData[i]['status']])
+                                                        .replace('${order_date}', testData[i]['order_date'])
+                                                        .replace('${products}', productsTest)
+                                                        .replace('${total_price}', totalPriceTest)
+                                                        .replace('${complete_date}', testData[i]['complete_date']);
+        
+        
+
+
+
+
+    }  
+}
+
+function searchOrder() {
+   let x =  sendRequestGET('http://localhost/api/ordervendors.php?vendor_id=' + vendor_id + "&search=order_id:" + document.getElementById('search').value);
+    console.log(x);
 }
